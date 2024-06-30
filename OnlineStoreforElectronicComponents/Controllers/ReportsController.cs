b5216ad52c10f22a8b8ca8b611f2f9ec8e1fdf23
Алексей <vasinlesha12345@mail.ru>
2024-06-30 17:43:20 +0300

    Top Five Selling Components fix

--- OnlineStoreforElectronicComponents/Controllers/ReportsController.cs
+++ OnlineStoreforElectronicComponents/Controllers/ReportsController.cs
@@ -8,12 +8,12 @@ public class ReportsController : Controller
     {
         _reportRepository = reportRepository;
     }
-    // GET: ReportsController
+    
     public async Task<ActionResult> TopFiveSellingComponents(DateTime? sDate = null, DateTime? eDate = null)
     {
         try
         {
-            // by default, get last 7 days record
+         
             DateTime startDate = sDate ?? DateTime.UtcNow.AddDays(-7);
             DateTime endDate = eDate ?? DateTime.UtcNow;
             var topFiveSellingComponents = await _reportRepository.GetTopNSellingComponentsByDate(startDate, endDate);
